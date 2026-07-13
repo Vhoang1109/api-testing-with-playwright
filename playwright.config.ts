805d29d3201ad3095dd8ba0bf5153b5763da13f2
Hoang <vhoang1109@gmail.com>
2026-07-13 15:05:27 +0700

    git commit -m "test: ignore demo tests in CI"

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,6 +24,14 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+ 
+   testIgnore: [
+        "**/lesson-12/other-style/demo-03.spec.ts",
+        "**/lesson-12/practice/01-simple-get.spec.ts",
+        "**/lesson-14/01-visual-comparisions.spec.ts",
+        "**/lesson-14/02-screenshot-mask.spec.ts",
+    ],
+
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
