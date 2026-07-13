7d02686f87f73a5086fe3c42715d7990ca819d47
Hoang <vhoang1109@gmail.com>
2026-07-13 15:28:37 +0700

    git commit -m "update file config"

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,12 +25,12 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
  
-  //  testIgnore: [
-  //       "**/lesson-12/other-style/demo-03.spec.ts",
-  //       "**/lesson-12/practice/01-simple-get.spec.ts",
-  //       "**/lesson-14/01-visual-comparisions.spec.ts",
-  //       "**/lesson-14/02-screenshot-mask.spec.ts",
-  //   ],
+   testIgnore: [
+        "**/lesson-12/other-style/demo-03.spec.ts",
+        "**/lesson-12/practice/01-simple-get.spec.ts",
+        "**/lesson-14/01-visual-comparisions.spec.ts",
+        "**/lesson-14/02-screenshot-mask.spec.ts",
+    ],
 
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
